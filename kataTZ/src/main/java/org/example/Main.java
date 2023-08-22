19b3477071926963b11fcd8846d12b329bdc7bb7
Pavel Bodrug <plkoij73@gmail.com>
2023-08-22 10:49:48 +0300

    version 1.0

--- kataTZ/src/main/java/org/example/Main.java
+++ kataTZ/src/main/java/org/example/Main.java
@@ -1,22 +1,62 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static org.example.RomanConverter.toArabic;
 import static org.example.RomanConverter.toRoman;
 
-
 public class Main {
+    final public static int FIRST_INDEX = 0;
+    final public static int OPERATION_INDEX = 1;
+    final public static int LAST_INDEX = 2;
+    final public static String[] ARITHMETICS = {"+", "-", "*", "/"};
+
     public static void main(String[] arg) {
         String input = new Scanner(System.in).nextLine();
-            int x;
-        try{
-            x = Integer.parseInt(input);
-            System.out.println(toRoman(x));
-        }catch (NumberFormatException e){
-            x = toArabic(input);
-            System.out.println("оу, вы решили ввести римские)");
-            System.out.println(x);
+
+        System.out.println(calc(input));
+    }
+
+    public static String calc(String input) {
+        int firstNumber;
+        int lastNumber;
+        int result;
+        // Делим выражение на строки разделенные пробелом
+        String[] exp = input.split("\\s+");
+        inputCorrect(exp);
+        String operation = exp[OPERATION_INDEX];
+        try {
+            // Пробуем сначала парсить в числа
+            firstNumber = Integer.parseInt(exp[FIRST_INDEX]);
+            lastNumber = Integer.parseInt(exp[LAST_INDEX]);
+            result = calcArabic(firstNumber, lastNumber, operation);
+            return "" + result;
+        } catch (NumberFormatException e) {
+            // Если не получилось, пробуем работать с римсими
+            firstNumber = toArabic(exp[FIRST_INDEX]);
+            lastNumber = toArabic(exp[LAST_INDEX]);
+            result = calcArabic(firstNumber, lastNumber, operation);
+            return toRoman(result);
         }
+    }
+// Метод проверки на корректность ввода
+    public static void inputCorrect(String[] input) {
+        if (input.length != 3)
+            if (input.length == 1) throw new IllegalArgumentException("Строка не является математической операцией.");
+            else
+                throw new IllegalArgumentException("Введен не верный формат выражения.");
+
+        if (!Arrays.stream(ARITHMETICS).toList().contains(input[OPERATION_INDEX]))
+            throw new IllegalArgumentException("Присутствует не существующая операция.");
+    }
+    // Метод расчета
+    public static int calcArabic(int firstNumber, int lastNumber, String operation) {
+        return switch (operation) {
+            case "+" -> firstNumber + lastNumber;
+            case "-" -> firstNumber - lastNumber;
+            case "*" -> firstNumber * lastNumber;
+            default -> firstNumber / lastNumber;
+        };
     }
 }
--- kataTZ/src/main/java/org/example/RomanConverter.java
+++ kataTZ/src/main/java/org/example/RomanConverter.java
@@ -2,25 +2,33 @@ package org.example;
 
 import java.util.Arrays;
 
-public class RomanConverter {
+class RomanConverter {
     // Массивы с римскими и арабскими числами
-    private static final String[] romanNumerals = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-    private static final int[] arabicNumerals = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    private static final String[] ROMAN_NUMERALS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private static final int[] ARABIC_NUMERALS = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final Character[] NUMERALS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     // Метод для конвертации из римских чисел в арабские
     public static int toArabic(String romanNumeral) {
         romanNumeral = romanNumeral.toUpperCase();
-        for(char s:romanNumeral.toCharArray())
-            if(!Arrays.stream(romanNumerals).toList().contains(""+s)) throw new IllegalArgumentException("ввел какую-то дичь");
+        // Ищем не существующие символы
+        for (char s : romanNumeral.toCharArray())
+            if (!Arrays.stream(ROMAN_NUMERALS).toList().contains("" + s)) {
+                if (Arrays.stream(NUMERALS).toList().contains(s)) { // Посмотрим, нет ли арабских цифр
+                    throw new IllegalArgumentException("Введены разные системы счисления.");
+                }
+                throw new IllegalArgumentException("Введено несуществующее число.");
+            }
 
         int result = 0;
         int i = 0;
 
         // Проверяем каждую римскую цифру и добавляем соответствующее арабское число
-        while (i < romanNumerals.length) {
-            if (romanNumeral.startsWith(romanNumerals[i])) {
-                result += arabicNumerals[i];
-                romanNumeral = romanNumeral.substring(romanNumerals[i].length());
+        while (i < ROMAN_NUMERALS.length) {
+            if (romanNumeral.startsWith(ROMAN_NUMERALS[i])) {
+                result += ARABIC_NUMERALS[i];
+                // И отсекаем обработанное
+                romanNumeral = romanNumeral.substring(ROMAN_NUMERALS[i].length());
             } else {
                 i++;
             }
@@ -31,13 +39,14 @@ public class RomanConverter {
 
     // Метод для конвертации из арабских чисел в римские
     public static String toRoman(int arabicNumeral) {
+        if (arabicNumeral<=0) throw new IllegalArgumentException("В римской системе счисления отсутствуют числа меньше 1.");
         StringBuilder result = new StringBuilder();
 
         // Проверяем каждое арабское число и добавляем соответствующую римскую цифру
-        for (int i = 0; i < arabicNumerals.length; i++) {
-            while (arabicNumeral >= arabicNumerals[i]) {
-                result.append(romanNumerals[i]);
-                arabicNumeral -= arabicNumerals[i];
+        for (int i = 0; i < ARABIC_NUMERALS.length; i++) {
+            while (arabicNumeral >= ARABIC_NUMERALS[i]) {
+                result.append(ROMAN_NUMERALS[i]);
+                arabicNumeral -= ARABIC_NUMERALS[i];
             }
         }
 
